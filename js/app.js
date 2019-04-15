a1b44021ce3eff2d718d686181f1e6242e09d5ae
joseph <joseph.hangarter@yahoo.com>
2019-04-14 20:52:37 -0700

    feature 2 and 3 completed

--- js/app.js
+++ js/app.js
@@ -1,48 +1,3 @@
-// 'use strict';
-
-// const all_animals = [];
-
-// function Animal (horn){
-//    this.image_url = horn.image_url;
-//    this.title = horn.title;
-//    this.description = horn.description;
-//    this.keyword = horn.keyword;
-//    this.horns = horn.horns;
-//  all_animals.push(this);
-// }
-
-// Animal.get_animal_data = function(){
-//   $.get('../data/page-1.json', 'json').then( data => {
-//     data.forEach(horn => new Animal(horn));
-//     all_animals.forEach(horn => horn.render());
-//   })
-// }
-
-// Animal.prototype.render = function(){
-//  const photo_section_html = $('#photo-template').html()
-
-//  $('main').append('<section id="clone"></section>');
-//  $('#clone').html(photo_section_html);
-//  $('#clone').find('h2').text(this.name);
-//  $('#clone').find('img').attr('src', this.image_url);
-//  $('#clone').find('p').text(this.title).text(this.description);
-//  $('#clone').attr('id', this.keyword);
-//  $('#clone').attr('class', this.keyword);
-// }
-
-// const test_animal = new Animal({});
-// test_animal.render();
-
-// $('#horned_animals').on('change', function() {
-//   let select_value = $(this).val();
-//   $('section').hide()
-//   $(`.${select_value}`).show();
-//   console.log(select_value);
-// });
-
-
-// Animal.get_animal_data();
-
 'use strict';
 
 let all_creatures = [];
@@ -67,16 +22,17 @@ Creature.prototype.render = function(){
 }
 
 Creature.prototype.make_option = function(){
-  if($(`option[value=${this.keyword}]`).length) return;
+  if ($(`option[value=${this.keyword}]`).length) return;
+  
   const new_html = creature_option_template(this);
   $('select').append(new_html);
 }
 
 const get_creature_data = data => {
   $.get(`${data}`, 'json').then(data => {
-    data.forEach( val => all_creatures.push(new Creature(val)));
-    all_creatures.forEach(creature => creature.render());
-    all_creatures.forEach(creature => creature.make_option())
+    data.forEach(val => all_creatures.push(new Creature(val)));
+    all_creatures.forEach(creature => {creature.render()});
+    all_creatures.forEach(creature => {creature.make_option()})
   })
 }
 
@@ -84,30 +40,13 @@ const get_creature_data = data => {
 
 $(document).ready(() => {
   get_creature_data('../data/page-1.json');
-
-  // $('select').on('change', function(){
-  //   all_creatures = [];
-  //   $('#deck').empty();
-  //   console.log($(this).text());
-  //   get_creature_data(`${$(this).text()}.json`);
-  // })
-
   $('select').on('change', function() {
-  let select_value = $(this).val();
-  $('main').empty()
-  $(`.${select_value}`).show();
-  console.log(select_value);
+    let select_value = $(this).val();
+    $('.card').hide();
+    let chosen = $('.card').filter(function(index, item) {
+      return $(item).attr('data-keyword') === select_value; //condition
+    });
+    chosen.show();
+    $('.card').css("margin-left", "auto").css("margin-right", "auto");
   });
 });
-// $('select').on('click', 'p.big', function() {
-//   all_creatures.sort((a,b) => {
-//     if(a.title > b.title) {
-//       return 1
-//     }
-//     if( a.title < b.title) return -1;
-//     return 0;
-//   })
-//   $('main').empty();
-//   all_creatures.forEach(creature => creature.render());
-//   })
-// })
